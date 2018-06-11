b1b0cd58a17c4f3f5315ba8c9d30df7748f68505
EthanZhang <ethan.zhang@orig3n.com>
2018-06-11 15:44:32 -0400

    misc fixes

--- src/main/java/com/demo/appserver/controller/UserController.java
+++ src/main/java/com/demo/appserver/controller/UserController.java
@@ -1,13 +1,11 @@
 package com.demo.appserver.controller;
 
 import javax.security.auth.message.callback.PrivateKeyCallback.Request;
-import javax.transaction.Status;
 
 import com.demo.appserver.model.AppUser;
 import com.demo.appserver.repository.AppUserRepository;
 import com.demo.appserver.model.SignUpResponse;
 
-import org.apache.coyote.Response;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
